cd9b22111d61af92ed3b6efe231c8238fd965169
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-05-13 19:25:44 +0100

    starting to turn html into js modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 console.log("this website is not responsive")
+
+//variables to hold all the html
+const mainDocument = document.querySelector('#container');
+const navbar = document.createElement('navbar');
+navbar.id = 'navbar';
+
+const logo = document.createElement('p');
+const list = document.createElement('ul');
+list.classList.add('nav-list');
+
+const menuElements = ['about', 'menu', 'contacts'];
+
+for (let el = 0; el < menuElements.length; el++) {
+    let li = document.createElement('li');
+    let anchor = document.createElement('a');
+
+    anchor.innerText = `${menuElements[el]}`;
+    anchor.id = `${menuElements[el]}`;
+    anchor.classList.add('nav-link');
+    anchor.href = '#';
+
+    li.appendChild(anchor);
+    list.appendChild(li);
+}
+
+logo.innerText = 'Restaurant Lorem';
+logo.classList.add('logo');
+
+navbar.appendChild(logo);
+navbar.appendChild(list);
+
+mainDocument.appendChild(navbar);
